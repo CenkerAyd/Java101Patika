d6fad041672d7456e13007285cd33f1282d103fc
cenk1903 <cenkeraydincse@gmail.com>
2022-12-25 19:43:56 +0300

    Create ArraysFrekans.java

---
+++ ArraysFrekans.java
@@ -0,0 +1,21 @@
+public class ArraysFrekans {
+   /* public static void main(String[] args) {
+        int[] array = {10, 30, 20, 30, 20, 5, 10, 20, 10, 10, 5};
+        int[] checked = new int[array.length];
+        int checkIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+     //       if (array[i]==)
+        }
+    }
+
+    static int countEleman(int[]array,int value){
+        int count=0;
+        for (int i:array){
+            if (value==i)
+                count++;
+        }
+        return count;
+    }
+
+    */
+}
