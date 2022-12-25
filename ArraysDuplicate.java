871443d9f496592353eb47d39d11a71619b88376
cenk1903 <cenkeraydincse@gmail.com>
2022-12-25 19:46:14 +0300

    Create ArraysDuplicate.java

---
+++ ArraysDuplicate.java
@@ -0,0 +1,23 @@
+public class ArraysDuplicate {
+   /* public static void main(String[] args) {
+        int[]array={3,7,3,3,2,9,10,21,1,33,9};
+        int[]duplicate=new int[array.length];
+        int start=0;
+        for (int i=0;i<array.length;i++){
+            for (int j=0;j<array.length;j++){
+                if ((i != j)&&(array))
+            }
+        }
+    }
+    static boolean isFind(int[]arr,int value){
+        for (int i:arr){
+            if (i==value){
+                return  true;
+            }
+        }
+        return false;
+    }
+
+    */
+
+}
